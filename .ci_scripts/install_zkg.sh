922a030718f60340316fa42d2a56d6b7d5f6bd72
Vlad Grigorescu <grigorescu@gmail.com>
2020-09-03 23:32:49 -0500

    Redo GitHub tests

--- .ci_scripts/install_zkg.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export PATH=/usr/local/zeek/bin:/opt/zeek/bin:/opt/zeek-nightly/bin:$PATH
-
-pip3 install -U sphinx_rtd_theme zkg
-zkg autoconfig
--- .ci_scripts/test.sh
+++ .ci_scripts/test.sh
@@ -2,27 +2,12 @@
 
 set -e
 
-function debug_and_die {
-    OUTPUT_PATH=$HOME/.zkg/testing/external_dns/clones/external_dns
-    if [ -s $OUTPUT_PATH/zkg.test_command.stdout ]; then
-	echo "zkg test command stdout"
-	echo "-----------------------"
-	cat $OUTPUT_PATH/zkg.test_command.stdout
-    fi
-    
-    if [ -s $OUTPUT_PATH/zkg.test_command.stderr ]; then
-	echo "zkg test command stderr"
-	echo "-----------------------"
-	cat $OUTPUT_PATH/zkg.test_command.stderr
-    fi
-    
-    exit 1
-}
+export PATH=/usr/local/zeek/bin:$PATH
 
-export PATH=/usr/local/zeek/bin:/opt/zeek/bin:/opt/zeek-nightly/bin:$PATH
+cd $GITHUB_WORKSPACE/tests
 
-echo "Running zkg test..."
-zkg test "$PWD" || debug_and_die
-echo "Tests succeeded. Running zkg install..."
-zkg install --force --skiptests "$PWD" || debug_and_die
-echo "Install succeeded."
+if scl -l; then
+    scl enable devtoolset-7 make
+else
+    make
+fi
